2575d85ba7c7b1fef2320407d1c11af5a4396baf
wojtek-jarosz <44193345+wojtek-jarosz@users.noreply.github.com>
2020-11-27 16:14:31 +0100

    Update config.js to use docs-only mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,29 +18,11 @@ module.exports = {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
       },
-      items: [
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-        },
-      ],
+      items: [],
     },
     footer: {
       style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Docs portal',
-              to: 'docs/',
-            },
-          ],
-        },
-       
-      ],
+      links: [],
       copyright: `Copyright © ${new Date().getFullYear()} Jorsek, Inc. Built with Docusaurus.`,
     },
   },
@@ -49,15 +31,10 @@ module.exports = {
       '@docusaurus/preset-classic',
       {
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl: 'http://easydita.com',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
