2fdd916da949db38f3f45a33a840296a513c804f
wojtek-jarosz <44193345+wojtek-jarosz@users.noreply.github.com>
2021-05-20 12:43:39 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,13 +43,13 @@ module.exports = {
       },
     ],
   ],
-  plugins: [
-    [require.resolve('@cmfcmf/docusaurus-search-local'), {
-        indexDocs: true,
-        docsRouteBasePath: '/',
-        indexDocSidebarParentCategories: 3,
-        indexPages: false,
-        language: "en"
-    }]
-  ],
+//   plugins: [
+//     [require.resolve('@cmfcmf/docusaurus-search-local'), {
+//         indexDocs: true,
+//         docsRouteBasePath: '/',
+//         indexDocSidebarParentCategories: 3,
+//         indexPages: false,
+//         language: "en"
+//     }]
+//   ],
 };
